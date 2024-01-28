8072b31e26db9b9c5940ff99e11d23cdaf8ed4f7
carolliu2024 <carolliu2024@u.northwestern.edu>
2024-01-28 14:06:00 -0600

    blank project

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,15 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <style> {/* font family */}
+        @import url('https://fonts.googleapis.com/css2?family=Questrial&display=swap');
+      </style>
+      <p>Questrial font. Welcome!</p>
+    </main>
+
+    
   );
 }
